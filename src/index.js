aba61c2cff502ecff6884632d4dac669a5a3df0e
cay <cronosstar007@gmail.com>
2025-09-07 22:24:02 +0700

    update penambahan cors

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import express from 'express';
 import path from 'path';
 import url from 'url';
 import routes from './routes/index.js';
+import cors from 'cors'
+
 
 const app = express()
 const port = 3000
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET','POST','PUT','DELETE'],
+    credetials: true
+}))
 
 // Middleware untuk parsing JSON
 app.use(express.json());
