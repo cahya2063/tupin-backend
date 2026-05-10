3b12aacda6d89da00db01974a5e5b0eb93c3cb4c
Muhamad Cahya Yunizar <cronosstar007@gmail.com>
2026-05-10 12:32:44 +0700

    tambah fitur managemenet skills

--- src/controllers/skills.js
+++ src/controllers/skills.js
@@ -1,5 +1,55 @@
 import SkillCollection from "../models/skills.js"
 
+const createSkills = async(req, res, next)=>{
+    try {
+        const {data} = req.body
+        await SkillCollection.create(data)
+        res.status(200).json({
+            success: true,
+            message: 'berhasil menambah skill baru'
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+const updateSkills = async(req, res,next)=>{
+    try {
+        const {skillId} = req.params
+        const {label} = req.body
+        const skill = await SkillCollection.findByIdAndUpdate(
+            skillId,
+            { label },
+            { new: true, runValidators: true }
+        )
+
+        if (!skill) {
+            return res.status(404).json({
+                message: 'Skill tidak ditemukan'
+            })
+        }
+
+        res.status(200).json({
+            message: 'Skill berhasil diupdate',
+            data: skill
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const deleteSkills = async(req, res, next)=>{
+    try {
+        const {skillId} = req.params
+        const deletedSkill = await SkillCollection.findByIdAndDelete(skillId)
+
+        res.status(200).json({
+            success: true,
+            message: 'berhasil hapus skill'
+        })
+    } catch (error) {
+        next(error)
+    }
+}
 const getAllSkills = async(req, res, next)=>{// client
     try {
         const data = await SkillCollection.find({})
@@ -24,4 +74,4 @@ const getSkillById = async(req, res, next)=>{
     }
 }
 
-export {getAllSkills, getSkillById}
+export {getAllSkills, getSkillById, createSkills, updateSkills, deleteSkills}
--- src/routes/skills.js
+++ src/routes/skills.js
@@ -1,11 +1,14 @@
 import express from 'express'
-import { getAllSkills, getSkillById } from '../controllers/skills.js'
+import { createSkills, deleteSkills, getAllSkills, getSkillById, updateSkills } from '../controllers/skills.js'
 import { authRole } from '../middleware/auth.js'
 
 const skillRouter = express.Router()
 
 skillRouter.get('/',getAllSkills)
 skillRouter.get('/:id', authRole(['client', 'admin']), getSkillById)
+skillRouter.post('/create-skills', authRole(['admin']), createSkills)
+skillRouter.post('/:skillId/update-skills', authRole(['admin']), updateSkills)
+skillRouter.delete('/:skillId/delete-skills', authRole(['admin']), deleteSkills)
 export {skillRouter}
 
 
